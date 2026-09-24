b1771395333bffbdbb5ab8c5839cd8ca279a3bb9
Hongda Jiang <jianghd@pku.edu.cn>
2026-09-24 10:03:28 +0800

    Use step 12000 LoRA by default in world-model scripts

--- scripts/world_model/run_world_model_step.py
+++ scripts/world_model/run_world_model_step.py
@@ -31,7 +31,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument(
         "--video-lora",
         type=Path,
-        default=DEFAULT_VIDEO_MODEL / "33000_lora.safetensors",
+        default=DEFAULT_VIDEO_MODEL / "12000_lora.safetensors",
     )
     parser.add_argument(
         "--output-dir",
